559afe8dfa3de5239207ccc11f3e4d346625cd4e
markjaniczak <markjaniczak@outlook.com>
2016-04-01 23:28:25 +1000

    Overhauled image processing to use html templating. Included the windows binary for rendering template as an image however the utility is cross platform.

--- ShinyChromeShower.py
+++ ShinyChromeShower.py
@@ -1,18 +1,18 @@
  # -*- coding: utf-8 -*-
-from PIL import Image, ImageDraw, ImageFont
 import time, praw, urllib, requests, os
 from io import BytesIO
-#from jinja2 import Environment, FileSystemLoader
-#import subprocess
+from jinja2 import Environment, FileSystemLoader
+import subprocess
 
 #######     SETTINGS        #######
-FONT = "Roboto-Light.ttf"
+FONT = "Roboto"
+FONT_URL = "https://fonts.googleapis.com/css?family=Roboto"
 IMAGE_SUBREDDIT = "EarthPorn"
 TEXT_SUBREDDIT = "Showerthoughts"
 LIM = 10
 MIN_RESOLUTION = (1920,1080)
 #Tolerence is out of 0 to 100 (Percentage)
-ASPECT_TOLERENCE = 18
+ASPECT_TOLERENCE = 50
 ###################################
 
 reddit = praw.Reddit(user_agent="Chromecast_Showerthoughts")
@@ -36,68 +36,50 @@ def get_image_size(url):
     return float(im.size[0]), float(im.size[1])
 
 def verify_image_sizing(img_H, img_W, min_RES, tolerance):
-	#Returns boolean value of whether image being pulled conforms to contraints.
-
-	#Set default response
-	verified = False
-	
-	#Ensure Tolerance value is within required range (0-100)
-	if tolerance >= 0 and tolerance <= 100:
-		tolerance = tolerance / 100.0
-	else:
-		print "Tolerence entered does not meet requirements, must be a whole value between 0-100. Quit script."
-		quit()
-
-	#Load H and W into seperate variables
-	min_RES_W = float(min_RES[0])
-	min_RES_H = float(min_RES[1])
-
-	#Get aspect decimal for image being pulled and pixel count
-	img_aspect = float(img_H)/float(img_W)
-	img_pixel = img_H*img_W
-
-	#Get high and low for tolerance against aspect ratio's
-	req_aspect_min = (min_RES_H/min_RES_W) * ( 1 - tolerance)
-	req_aspect_max = (min_RES_H/min_RES_W) * (1 + tolerance)
-
-	#Get minimum pixel count in image
-	req_pixel_min = min_RES_H*min_RES_W
-
-	if req_aspect_min <= img_aspect and img_aspect <= req_aspect_max and img_pixel >= req_pixel_min:
-		#Image is within tolerance specified and has more pixels then the min stated.
-		verified = True
-	
-	return verified	
-
-def multiline_text(text, image_width, image_height):
     """
-    Splits large text up into multiple lines by using newlines so
-    that it fits onto the given image dimensions.
-    The text is to fit within 2/3 of the image width. There
-    is currently no restraint on the number of lines so it can spill
-    off the image. The font size is dependent on the image height -
-    it's 3% of the height. This is an arbitrary value and still
-    being decided.
+    Verifies if an image is suitable for use by checking the aspect ratio
+    and minimum resolution.
 
     Args:
-        text (string): the single line text to fit into multiple lines
-        image_width (int): the width of the image to fit the text onto
-        image_height (int): the height of the image to fit the text onto
-
+        img_H (int): Height of image
+        img_W (int): Width of image
+        min_RES (int): Minimum resolution allowable for use
+        tolerance (int): tolerance of image constraints in percentage
     Returns:
-        string: the given text with added newlines
+        Boolean: True if the image is suitable, false otherwise
     """
-    tail = text
-    length = 0
-    while font.getsize(tail)[0] > 2*image_width/3:
-        head = tail
-        while font.getsize(head)[0] > 2*image_width/3:
-            head = head.rsplit(' ', 1)[0]
-        length += len(head)
-        tail = tail[length:]
-        text = text[:length] + '\n' + text[length:]
-        length += len("\n")
-    return text
+    "Set default response"
+    verified = False
+
+    "Ensure Tolerance value is within required range (0-100)"
+    if tolerance >= 0 and tolerance <= 100:
+        tolerance = tolerance / 100.0
+    else:
+        print """Tolerance entered does not meet requirements, must be a whole
+        value between 0-100. Quit script."""
+        quit()
+
+    "Load H and W into seperate variables"
+    min_RES_W = float(min_RES[0])
+    min_RES_H = float(min_RES[1])
+
+    "Get aspect decimal for image being pulled and pixel count"
+    img_aspect = float(img_H)/float(img_W)
+    img_pixel = img_H*img_W
+
+    "Get high and low for tolerance against aspect ratios"
+    req_aspect_min = (min_RES_H/min_RES_W) * ( 1 - tolerance)
+    req_aspect_max = (min_RES_H/min_RES_W) * (1 + tolerance)
+
+    "Get minimum pixel count in image"
+    req_pixel_min = min_RES_H*min_RES_W
+
+    if (req_aspect_min <= img_aspect and img_aspect <= req_aspect_max
+        and img_pixel >= req_pixel_min):
+        """Image is within tolerance specified and has more pixels
+        then the min stated."""
+        verified = True
+    return verified
 
 def fill_template_url(template, image_url, text):
     """
@@ -109,45 +91,24 @@ def fill_template_url(template, image_url, text):
         text (string): the text to put in the centre of the image
     """
     template = env.get_template(template)
-    output = template.render(image=image_url, showerthought=text)
-    with open("test.html", "wb") as fh:
+    output = template.render(font=FONT, font_url=FONT_URL, image=image_url,
+        showerthought=text, height=MIN_RESOLUTION[1], width=MIN_RESOLUTION[0])
+    with open("temp.html", "wb") as fh:
         fh.write(output)
 
 def capture_template(filename):
-    return subprocess.call(r"wkhtmltopdf\bin\wkhtmltoimage --height 1080" +
-    "--width 1920 test.html" + os.getcwd() + "\\" + filename , shell=True)
-
-def draw_my_text(image, text):
     """
-    Draws the text over the given image.
+    Uses webkit to render the processed html file and saves as an image.
 
-    Args:
-        image (ImageDraw): the actual image to draw the text over. note that
-            this is not the file but the actual ImageDraw object created
-            from the image file.
-        text (string): the text to draw over the image.
-        w (int): this is the x position of where to put the top left corner of
-            the text. Different for single line and multiline text.
-        h (int): this is the y position of where to put the top left corner of
-        the text.
-
+    Returns:
+        int: 0 if capture is successful, positive int otherwise
     """
-    h = (image.size[1] - ((text.count('\n')+1) * (font.getsize(text)[1] + 5)))/2
-    if text.count('\n') > 0:
-        w = image.size[0]/6
-    else:
-        w =(image.size[0]-font.getsize(text)[0])/2
-    draw.multiline_text((w + 1, h + 1), text, font=font,
-                        align='center', spacing=5, fill="black")
-    draw.multiline_text((w - 1, h + 1), text, font=font,
-                        align='center', spacing=5, fill="black")
-    draw.multiline_text((w + 1, h - 1), text, font=font,
-                        align='center', spacing=5, fill="black")
-    draw.multiline_text((w - 1, h - 1), text, font=font,
-                        align='center', spacing=5, fill="black")
-    draw.multiline_text((w, h), text, font=font,
-                        align='center', spacing=5, fill="white")
-    img.save(imageName, "JPEG", quality=100, optimize=True, progressive=True)
+    print (r"wkhtmltopdf\bin\wkhtmltoimage --height " +
+    str(MIN_RESOLUTION[1]) + " --width " + str(MIN_RESOLUTION[0]) + " --quality 100"
+    + " temp.html " + os.getcwd() + filename)
+    return subprocess.call(r"wkhtmltopdf\bin\wkhtmltoimage --height " +
+    str(MIN_RESOLUTION[1]) + " --width " + str(MIN_RESOLUTION[0]) + " --quality 100"
+    + " temp.html " + os.getcwd() + filename)
 
 if __name__ == "__main__":
     for i in range(LIM):
@@ -159,18 +120,12 @@ if __name__ == "__main__":
             W, H = get_image_size(image.url)
         except IOError:
             continue
-
-	if verify_image_sizing(H, W, MIN_RESOLUTION, ASPECT_TOLERENCE) == True:
+    if verify_image_sizing(H, W, MIN_RESOLUTION, ASPECT_TOLERENCE) == True:
             "Label image using current date and image in sequence."
-            imageName = time.strftime("%d%m%y"+ str(i) + ".jpg")
-            urllib.urlretrieve(image.url, imageName)
-            "Wait for download"
-            time.sleep(15)
-            img = Image.open(imageName)
+            imageName = time.strftime(r"\%d%m%y"+ str(i) + ".jpg")
             thought = next(thoughts).title
-            font = ImageFont.truetype(FONT, int(H*.03))
-            draw = ImageDraw.Draw(img)
-            thought = multiline_text(thought, W, H)
-            draw_my_text(img, thought)
+            fill_template_url("template.html", image.url, thought)
+            capture_template(imageName)
+            os.remove("temp.html")
 
-os.system("uploadr.py")
+#os.system("uploadr.py")
